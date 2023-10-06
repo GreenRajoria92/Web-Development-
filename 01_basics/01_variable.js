df1a61895bb11e56827d1926c42b62bd3b818084
Green Rajoria <147193191+GreenRajoria92@users.noreply.github.com>
2023-10-06 21:41:54 +0000

    Config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "Green@gmail.com"
+var accountPassword = "123450"
+accountCity = "Ajmer"
+
+// accountId = 2
+
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "121212"
+accountCity = "Kota"
+
+console.table([accountEmail,accountCity,accountPassword])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"Use Strict"; // treat all JS code as newer version 
+
+
+//Code readablity should be extremely high 
+
+let name = "Green"
+let age = 18 
+let isLoggedIn = false 
+
+/* Number => 2 to Power 23 
+big int 
+string should be using under double Quotes
+boolean = true/false 
+null= Standalone value
+Undefined=value has not assigned
+symbol (mostly use for uniqueness)
+Object */
+
+console.log(typeof undefined );
